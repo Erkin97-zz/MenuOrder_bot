9863dccb9f6a0723c62c73c4d9c7a1d3de2fd5ac
(학생) ERKIN MATKAZIEV (전기전자컴퓨터공학부) <erkin97@unist.ac.kr>
2019-06-14 09:36:59 +0900

    implement menu

---
+++ main.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+var mainKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("Make Order 🍽"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("About ❔"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("Register cafe [Coming Soon] ⌛"),
+	),
+)
+
+var orderKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("Café Enjothie in UNIST 🇰🇷"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("Back 🔙"),
+	),
+)
+
+var menuFirstKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("Smoothies 🥤"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("Snacks 🥨"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("Back 🔙"),
+	),
+)
+
+var menuData = map[string][]string{
+	"Smoothies 🥤": []string{
+		"Strawberry Shortcake Smoothie 🍓",
+		"Apply Smoothie 🍏",
+		"Pineapple Smoothie 🍍",
+		"Back 🔙",
+	},
+	"Snacks 🥨": []string{
+		"Egg 🥚",
+		"Cookie 🍪",
+		"Salad 🥗",
+		"Back 🔙",
+	},
+}
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("MENU"))
+	if err != nil {
+		log.Printf("Failed to connect with bot. Error: %v\n", err)
+		return
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+	state := make(map[string]string)
+	category := make(map[string]string)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		messageText := update.Message.Text
+		currentState := state[update.Message.From.UserName]
+
+		log.Printf("[%s] %s - state: %s", update.Message.From.UserName, update.Message.Text, currentState)
+
+		if currentState == "" || currentState == "main" { // main state
+			if messageText == "Make Order 🍽" {
+				msgText := "Choose restaurant"
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, msgText)
+				msg.ReplyMarkup = orderKeyboard
+				bot.Send(msg)
+				state[update.Message.From.UserName] = "order"
+			} else if messageText == "About ❔" {
+				msgText := fmt.Sprintln("Smart Menu ordering bot is the new system of simple and fast ordering.\nYou can choose any restaraunt" +
+					" from the list and make your orders\n")
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, msgText)
+				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+				msg.ReplyMarkup = mainKeyboard
+				bot.Send(msg)
+				state[update.Message.From.UserName] = "main"
+			} else {
+				msgText := fmt.Sprintf("Hello, %s!\nWelcome to the Smart Menu ordering bot.\n", update.Message.From.FirstName)
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, msgText)
+				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+				msg.ReplyMarkup = mainKeyboard
+				bot.Send(msg)
+				state[update.Message.From.UserName] = "main"
+			}
+
+		} else if currentState == "order" {
+			if messageText == "Café Enjothie in UNIST 🇰🇷" {
+				msgText := fmt.Sprintln("Choose category")
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, msgText)
+				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+				msg.ReplyMarkup = menuFirstKeyboard
+				bot.Send(msg)
+				state[update.Message.From.UserName] = "category"
+			} else if messageText == "Back 🔙" {
+				msgText := fmt.Sprintf("Hello, %s!\nWelcome to the Smart Menu ordering bot.\n", update.Message.From.FirstName)
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, msgText)
+				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+				msg.ReplyMarkup = mainKeyboard
+				bot.Send(msg)
+				state[update.Message.From.UserName] = "main"
+			} else {
+				msgText := "Choose restaurant"
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, msgText)
+				msg.ReplyMarkup = orderKeyboard
+				bot.Send(msg)
+				state[update.Message.From.UserName] = "order"
+			}
+
+		} else if currentState == "category" {
+			if messageText == "Back 🔙" {
+				msgText := "Choose restaurant"
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, msgText)
+				msg.ReplyMarkup = orderKeyboard
+				bot.Send(msg)
+				state[update.Message.From.UserName] = "order"
+			} else if menuData[messageText] != nil {
+				msgText := "Please choose product"
+				var smoothieList [][]tgbotapi.KeyboardButton
+				smoothieList = make([][]tgbotapi.KeyboardButton, len(menuData[messageText]))
+
+				for index, drink := range menuData[messageText] {
+					smoothieList[index] = make([]tgbotapi.KeyboardButton, 1)
+					newButton := tgbotapi.NewKeyboardButton(drink)
+					smoothieList[index][0] = newButton
+				}
+				replyKeyboard := tgbotapi.NewReplyKeyboard(smoothieList...)
+				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, msgText)
+				msg.ReplyMarkup = replyKeyboard
+				bot.Send(msg)
+				state[update.Message.From.UserName] = "product"
+				category[update.Message.From.UserName] = messageText
+			} else {
+				msgText := fmt.Sprintln("Choose category")
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, msgText)
+				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+				msg.ReplyMarkup = menuFirstKeyboard
+				bot.Send(msg)
+				state[update.Message.From.UserName] = "category"
+			}
+		} else if currentState == "product" {
+			if messageText == "Back 🔙" {
+				msgText := fmt.Sprintln("Choose category")
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, msgText)
+				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+				msg.ReplyMarkup = menuFirstKeyboard
+				bot.Send(msg)
+				state[update.Message.From.UserName] = "category"
+			}
+		} else {
+			log.Println("roflan bug")
+			state[update.Message.From.UserName] = "main"
+		}
+	}
+}
